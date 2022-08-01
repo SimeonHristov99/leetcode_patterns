b2ecc740b57b221c8fbbd187a89e126c17cc1dc5
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-08-01 19:30:41 +0300

    Add task 37.

---
+++ task37/task37.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+        left = []
+        right = []
+
+        to_add = 1
+        for n in nums:
+            left.append(to_add)
+            to_add *= n
+
+        to_add = 1
+        for n in nums[::-1]:
+            right.insert(0, to_add)
+            to_add *= n
+
+        for (n, k) in zip(left, right):
+            result.append(n*k)
+
+        return result
